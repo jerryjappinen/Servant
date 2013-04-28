cacb628200af06955fcc557806d21c8e89d3367d
Jerry Jäppinen <eiskis@gmail.com>
2013-04-29 00:54:36 +0300

    Added barebones template

---
+++ templates/barebones/3-body.php
@@ -0,0 +1,3 @@
+<?php
+echo $servant->template()->content();
+?>
---
+++ templates/barebones/5-html-end.php
@@ -0,0 +1,15 @@
+<?php
+
+// Include scripts
+// FLAG I really shouldn't hardcode the name of read action...
+if ($servant->action()->id() === 'read') {
+	$temp = implode('/', $servant->site()->article()->tree()).'/';
+} else {
+	$temp = '';
+}
+echo '<script src="'.$servant->paths()->root('domain').$servant->site()->id().'/scripts/'.$temp.'"></script>';
+
+// End it all
+echo '</body></html>';
+
+?>
