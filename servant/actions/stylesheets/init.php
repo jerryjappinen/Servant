ccf2d9b87a3070244c960995efea79a9b9b3790d
Jerry Jäppinen <eiskis@gmail.com>
2013-05-01 22:14:48 +0300

    Stylesheet action parses stylesheet formats dynamically based on settings

--- servant/actions/stylesheets/init.php
+++ servant/actions/stylesheets/init.php
@@ -1,6 +1,7 @@
 <?php
 
 // We need this to manipulate URLs
+$servant->action()->contentType('css');
 $servant->utilities()->load('urls');
 
 ?>
--- servant/actions/stylesheets/run.php
+++ servant/actions/stylesheets/run.php
@@ -1,9 +1,25 @@
 <?php
 
+/**
+* FLAG
+*   - Not very elegant or dynamic
+*/
+
+// Allowed superset file extensions, mapped to their format
+$allowedFormats = array();
+$temp = $servant->settings()->formats('stylesheets');
+unset($temp['css']);
+foreach ($temp as $type => $extensions) {
+	foreach ($extensions as $extension) {
+		$allowedFormats[$extension] = $type;
+	}
+}
+unset($temp, $type, $extensions, $extension);
+
 // All stylesheets for site go here
 $stylesheetSets = array(
-	array('language' => false, 'content' => ''),
-	array('language' => false, 'content' => ''),
+	'theme' => array('format' => false, 'content' => ''),
+	'site' => array('format' => false, 'content' => ''),
 );
 
 // We need this for URL manipulations
@@ -13,25 +29,26 @@ $actionsPath = $servant->paths()->root('domain').$servant->site()->id().'/';
 
 /**
 * Theme's style files
+*
+* If SCSS or LESS is used, the first such file determines the type used for the whole set. These cannot be mixed within one set.
 */
 foreach ($servant->theme()->stylesheets('plain') as $path) {
 
-	// Detect format (based on first superset file)
-	// FLAG shouldn't be using file extension directly
-	// FLAG hardcoding extensions
-	$language = pathinfo($path, PATHINFO_EXTENSION);
-	if (in_array($language, array('scss', 'less'))) {
+	// Special format is used
+	$extension = pathinfo($path, PATHINFO_EXTENSION);
+	if (array_key_exists($extension, $allowedFormats)) {
 
-		// Format is now selected for theme package
-		if (!$stylesheetSets[0]['language']) {
-			$stylesheetSets[0]['language'] = $language;
+		// Set's format has not been selected yet, we'll do it now
+		if (!$stylesheetSets['theme']['format']) {
+			$stylesheetSets['theme']['format'] = $allowedFormats[$extension];
 
-		// Fail if supersets are mixed
-		} else if ($stylesheetSets[0]['language'] !== $language) {
-			fail('SCSS and LESS cannot be mixed in a theme package');
+		// Mixing specia formats will fail
+		} else if ($stylesheetSets['theme']['format'] !== $allowedFormats[$extension]) {
+			fail('CSS preprocessor formats cannot be mixed in a theme package');
 		}
+
 	}
-	unset($language);
+	unset($extension);
 
 
 
@@ -42,7 +59,7 @@ foreach ($servant->theme()->stylesheets('plain') as $path) {
 	$relativeUrl = substr((dirname($path).'/'), strlen($servant->theme()->path('plain')));
 
 	// Get CSS file contents with URLs replaced
-	$stylesheetSets[0]['content'] .= manipulateCSSUrls(file_get_contents($servant->format()->path($path, 'server')), $rootUrl, $relativeUrl, $actionsPath);
+	$stylesheetSets['theme']['content'] .= manipulateCSSUrls(file_get_contents($servant->format()->path($path, 'server')), $rootUrl, $relativeUrl, $actionsPath);
 }
 
 
@@ -52,22 +69,23 @@ foreach ($servant->theme()->stylesheets('plain') as $path) {
 */
 foreach ($servant->site()->article()->stylesheets('plain') as $path) {
 
-	// Detect format (based on first superset file)
-	// FLAG shouldn't be using file extension directly
-	// FLAG hardcoding extensions
-	$language = pathinfo($path, PATHINFO_EXTENSION);
-	if (in_array($language, array('scss', 'less'))) {
+	// Special format is used
+	$extension = pathinfo($path, PATHINFO_EXTENSION);
+	if (array_key_exists($extension, $allowedFormats)) {
 
-		// Format is now selected for site's stylesheets
-		if (!$stylesheetSets[1]['language']) {
-			$stylesheetSets[1]['language'] = $language;
+		// Set's format has not been selected yet, we'll do it now
+		if (!$stylesheetSets['site']['format']) {
+			$stylesheetSets['site']['format'] = $allowedFormats[$extension];
 
-		// Fail if supersets are mixed
-		} else if ($stylesheetSets[1]['language'] !== $language) {
-			fail('SCSS and LESS cannot be mixed in site\'s stylesheets');
+		// Mixing specia formats will fail
+		} else if ($stylesheetSets['site']['format'] !== $allowedFormats[$extension]) {
+			fail('CSS preprocessor formats cannot be mixed in site styles');
 		}
+
 	}
-	unset($language);
+	unset($extension);
+
+
 
 	// Root is site directory root
 	$rootUrl = $servant->site()->path('domain');
@@ -76,7 +94,7 @@ foreach ($servant->site()->article()->stylesheets('plain') as $path) {
 	$relativeUrl = substr((dirname($path).'/'), strlen($servant->site()->path('plain')));
 
 	// Get CSS file contents with URLs replaced
-	$stylesheetSets[1]['content'] .= manipulateCSSUrls(file_get_contents($servant->format()->path($path, 'server')), $rootUrl, $relativeUrl, $actionsPath);
+	$stylesheetSets['site']['content'] .= manipulateCSSUrls(file_get_contents($servant->format()->path($path, 'server')), $rootUrl, $relativeUrl, $actionsPath);
 }
 
 
@@ -85,21 +103,26 @@ foreach ($servant->site()->article()->stylesheets('plain') as $path) {
 * Output
 */
 
-// Theme and site styles use the same superset language; parse as one
-// FLAG not very elegant or dynamic
-if ($stylesheetSets[0]['language'] and $stylesheetSets[0]['language'] === $stylesheetSets[1]['language']) {
-	$stylesheetSets[0]['content'] = $stylesheetSets[0]['content'].$stylesheetSets[1]['content'];
-	unset($stylesheetSets[1]);
+// Theme and site styles use the same superset format; parse as one (so variables from theme can be used in site styles, for example)
+if ($stylesheetSets['theme']['format'] and $stylesheetSets['theme']['format'] === $stylesheetSets['site']['format']) {
+	$stylesheetSets['theme']['content'] = $stylesheetSets['theme']['content'].$stylesheetSets['site']['content'];
+	unset($stylesheetSets['site']);
 }
 
-// Go through each set
+// Parse sets
 $output = '';
 foreach ($stylesheetSets as $stylesheetSet) {
 
-	// Superset language is used
-	if ($stylesheetSet['language']) {
-		$methodName = $stylesheetSet['language'].'ToCss';
-		$output .= $servant->parse()->$methodName($stylesheetSet['content']);
+	// Special format is used
+	if ($stylesheetSet['format']) {
+		$methodName = $stylesheetSet['format'].'ToCss';
+
+		// Parse if possible
+		if (method_exists($servant->parse(), $methodName)) {
+			$output .= $servant->parse()->$methodName($stylesheetSet['content']);
+		} else {
+			fail(strtoupper($stylesheetSet['format']).' stylesheets are not supported.');
+		}
 
 	// Raw CSS
 	} else {
@@ -111,6 +134,6 @@ foreach ($stylesheetSets as $stylesheetSet) {
 
 
 // We're done
-$servant->action()->contentType('css')->output(trim($output));
+$servant->action()->output(trim($output));
 
 ?>
