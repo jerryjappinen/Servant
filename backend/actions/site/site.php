34a043974b3399a06303142fb6e4b79748f3c482
Jerry Jäppinen <eiskis@gmail.com>
2013-11-30 23:35:46 +0200

    Using

--- backend/actions/site/site.php
+++ backend/actions/site/site.php
@@ -2,7 +2,7 @@
 
 // Nest the page action
 $pageAction = $action->nest('page');
-$output = $action->nestTemplate($servant->site()->template(), $pageAction->output());
+$output = $action->nestTemplate($page->template(), $pageAction->output());
 
 // Output page content
 $action->contentType('html')->contentType($pageAction->contentType())->output($output);
