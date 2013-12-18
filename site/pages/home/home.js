987d79ac4797e87c8ea1170c0e051f28b3f941d0
Jerry Jäppinen <eiskis@gmail.com>
2013-12-18 22:30:18 +0200

    Updated intro site

--- site/pages/home/home.js
+++ site/pages/home/home.js
@@ -2,6 +2,17 @@
 $(document).ready(function() {
 
 	// Change title in menu
-	$('.row-menu h1 a').wrapInner('<span class="hide-over-break"></span>').prepend('<span class="hide-under-break">servantframework.com</span>');
+	var homeLink = $('.row-menu h1 a');
+
+	homeLink.wrapInner('<span class="hide-over-break"></span>').prepend('<span class="hide-under-break">servantframework.com</span>');
+
+	homeLink.on('click', function (event) {
+		if (doc.scrollTop() > 0) {
+			event.preventDefault();
+			$('html, body').animate({
+				scrollTop: $('body').offset().top
+			}, 200);
+		}
+	});
 
 });
