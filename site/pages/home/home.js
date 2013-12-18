d76d59db794499169d6c2ab72d8ddb8a42ec43e8
Jerry Jäppinen <eiskis@gmail.com>
2013-12-18 12:06:00 +0200

    Updated intro site

--- site/pages/home/home.js
+++ site/pages/home/home.js
@@ -1,6 +1,7 @@
 
 $(document).ready(function() {
 
+	// Change title in menu
 	$('.row-menu h1 a').html('<span class="hide-over-break">About</span><span class="hide-under-break">servantframework.com</span>');
 
 });
