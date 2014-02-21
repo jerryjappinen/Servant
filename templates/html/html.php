d098ea65564f9622ee251504d4fef2e0ab1c054e
Jerry Jäppinen <eiskis@gmail.com>
2014-02-21 14:19:20 +0200

    Fixed typo in html template

--- templates/html/html.php
+++ templates/html/html.php
@@ -178,7 +178,7 @@ unset($temp, $pointer, $i, $value);
 		<?php echo $meta ?>
 
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-		<meta name="misapplication-tap-highlight" content="no">
+		<meta name="msapplication-tap-highlight" content="no">
         <meta name="apple-mobile-web-app-status-bar-style" content="black">
 		<style type="text/css">
 			@-ms-viewport{width: device-width;}
