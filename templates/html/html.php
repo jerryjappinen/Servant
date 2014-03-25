118b08647cc8e9d62b5a55d350ebf05e4d47618e
Jerry Jäppinen <eiskis@gmail.com>
2014-03-25 09:19:48 +0200

    Added minimal-ui meta tag

--- templates/html/html.php
+++ templates/html/html.php
@@ -177,7 +177,7 @@ unset($temp, $pointer, $i, $value);
 
 		<?php echo $meta ?>
 
-		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimal-ui">
 		<meta name="msapplication-tap-highlight" content="no">
         <meta name="apple-mobile-web-app-status-bar-style" content="black">
 		<style type="text/css">
